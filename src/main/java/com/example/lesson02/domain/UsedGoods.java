ad7bd4787c32d9f066b06ed4afa704ee5f058620
test <test@test.com>
2024-09-27 21:47:35 +0900

    240927

---
+++ src/main/java/com/example/lesson02/domain/UsedGoods.java
@@ -0,0 +1,68 @@
+package com.example.lesson02.domain;
+
+import java.time.LocalDateTime;
+
+// 일반 Java Bean 
+// DTO, domain, Entity 	
+public class UsedGoods {
+
+	// 필드 (자료 저장 공간) 
+	private int id;
+	private int sellerId;
+	private String title;
+	private String description;
+	private int price;
+	private String pictureUrl;
+	private LocalDateTime createAt;
+	private LocalDateTime updateAt;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getSellerId() {
+		return sellerId;
+	}
+	public void setSellerId(int sellerId) {
+		this.sellerId = sellerId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getPictureUrl() {
+		return pictureUrl;
+	}
+	public void setPictureUrl(String pictureUrl) {
+		this.pictureUrl = pictureUrl;
+	}
+	public LocalDateTime getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(LocalDateTime createAt) {
+		this.createAt = createAt;
+	}
+	public LocalDateTime getUpdateAt() {
+		return updateAt;
+	}
+	public void setUpdateAt(LocalDateTime updateAt) {
+		this.updateAt = updateAt;
+	}
+}
